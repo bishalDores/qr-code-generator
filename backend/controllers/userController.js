bf931835f688fb1df25f6d1cb052ce35a5a8818b
bishal <bishalpd@gmail.com>
2023-10-08 16:10:41 +0600

    generating qr code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
 const sendToken = require("../utils/jwtToken");
+const QRCode = require("qrcode");
 
 // get current user profile => api/v1/me
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
@@ -24,6 +25,31 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// generate QR Code  => api/v1/generateQR
+exports.generateQRCode = catchAsyncErrors(async (req, res, next) => {
+  const url = req.body.url;
+  if (url.length === 0) {
+    return next(new ErrorHandler("Url is not found", 404));
+  } else {
+    QRCode.toDataURL(url, function (err, url) {
+      res.status(200).json({
+        success: true,
+        data: url,
+      });
+    });
+  }
+});
+
+// get public user profile => api/v1/publicProfile
+exports.getPublicProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id).select("-__v");
+
   res.status(200).json({
     success: true,
     data: user,
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserProfile, updateProfile } = require("../controllers/userController");
+const { getUserProfile, updateProfile, generateQRCode, getPublicProfile } = require("../controllers/userController");
 const { isAuthenticated } = require("../middlewares/auth");
 
 router.route("/me").get(isAuthenticated, getUserProfile);
+router.route("/publicProfile/:id").get(getPublicProfile);
 router.route("/update").put(isAuthenticated, updateProfile);
+router.route("/generateQR").post(generateQRCode);
 
 module.exports = router;
