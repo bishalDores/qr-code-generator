c5aec7dd5f3416c18d83c501bfdf5e5ff2ca49c0
bishal <bishalpd@gmail.com>
2023-10-14 23:17:31 +0600

    client added

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <main>
+      <h2>This is heading</h2>
+      <p>Paragraph</p>
+      <Button variant="filled">Button</Button>
+    </main>
+  );
+}
