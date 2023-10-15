d65958c3d329d646ccf0d9ad2dbaf5c674248849
bishal <bishalpd@gmail.com>
2023-10-15 21:35:46 +0600

    home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,5 @@
 import NavLayout from "@/components/NavLayout";
-import { Container } from "@mantine/core";
+import { Button, Center } from "@mantine/core";
 
 export default function Home() {
   return (
@@ -7,21 +7,20 @@ export default function Home() {
       <NavLayout>
         <div className="landing_page_wrapper">
           <div className="inner">
-            <div>
-              <img src="/images/manifestation.svg" alt="cta image" />
-            </div>
-            <div>
-              <p>
-                "Manifestation" is a cutting-edge mobile application designed to empower users to visually represent
-                their digital identity with a single scan. By generating personalized QR codes, our app seamlessly
-                bridges the physical and digital worlds, fostering connections between individuals, businesses, and
-                organizations. With a simple scan, "Manifestation" directs users to their own dedicated profile page,
-                allowing them to curate and share vital information, achievements, and personal details. This innovative
-                platform embodies the concept of 'manifesting' one's online presence, offering a professional and
-                efficient means of conveying user information, fostering networking, and making a lasting impression in
-                a fast-paced, digitally interconnected world.
-              </p>
-            </div>
+            <p>
+              <span style={{ fontWeight: 600, fontSize: "22px" }}>&quot;Manifestation&quot;</span> is a cutting-edge web application designed to
+              empower users to visually represent their digital identity with a single scan. By generating personalized QR codes, our app seamlessly
+              bridges the physical and digital worlds, fostering connections between individuals, businesses, and organizations. With a simple scan,
+              &quot;Manifestation&quot; directs users to their own dedicated profile page, allowing them to curate and share vital information,
+              achievements, and personal details. This innovative platform embodies the concept of manifesting one&apos;s online presence, offering a
+              professional and efficient means of conveying user information, fostering networking, and making a lasting impression in a fast-paced,
+              digitally interconnected world.
+            </p>
+            <Center>
+              <Button component="a" href="/auth/login" mt={"lg"} color="violet.9">
+                START HERE
+              </Button>
+            </Center>
           </div>
         </div>
       </NavLayout>
